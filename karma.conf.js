43d06c3969cb45344b0d60bcd99b0874c0b6fc11
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-15 17:05:53 +0530

    Initial commit
    --/The Heart of Test System/-- of "MeltXpert®".

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,32 @@
 #!/usr/bin/env node
+
+
+/*!
+ * MeltXpert®
+ * Improved Nucleic Acid homo & hetero duplexes thermal behavior analysis program.
+ * ________________________________________________________________________________
+ *
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ________________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ________________________________________________________________________________
+ *
+ * @date      : 11-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : karma
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ________________________________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "MeltXpert®".
+ * ________________________________________________________________________________
+ */
