ddd72c76782a0979afb211f625b412bb8b9b26f3
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-15 17:10:29 +0530

    Initial commit
    --/The Heart of Test System/-- of "MeltXpert®".

--- config.rb
+++ config.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+
+
+# MeltXpert®
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+#     With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Umeå
+# @date       : 11-Nov-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
