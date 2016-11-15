dedd941f13ff22d167cb247c2def48917432bf54
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-15 16:18:40 +0530

    Initial commit
    SEED™: Build System — MeltXpert®.
    A Sequømics Product — http://sequomics.com/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,32 @@
 #!/usr/bin/env node
+
+
+/*!
+ * MeltXpert®
+ * Improved Nucleic Acid homo & hetero duplexes thermal behavior analysis program.
+ * ________________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ________________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ________________________________________________________________________________
+ *
+ * @date      : 11-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ________________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ________________________________________________________________________________
+ */
