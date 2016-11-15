ac6c0b1e6f4a71ff9359817f1299ec15d146499d
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-15 18:22:24 +0530

    Initial commit
    The Node.js® — File System.
    File I/O is provided by simple wrappers around standard POSIX functions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,3 +46,7 @@
 // Invoking strict mode.
 // @purpose: Strict mode applies to entire scripts or to individual functions.
 "use strict";
+
+// To load required Node module.
+//=-----------------------------
+var fs          = require('fs');
