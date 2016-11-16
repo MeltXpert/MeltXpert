37572a59d0880309f0919e1199c637342f06d4bd
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-16 10:18:11 +0530

    Initial commit
    SEED™: Document Generation System — MeltXpert®.
    A Sequømics Product — http://sequomics.com/.

---
+++ libs/assembly/convert.js
@@ -0,0 +1,29 @@
+/*!
+ * MeltXpert®
+ * Improved Nucleic Acid homo & hetero duplexes thermal behavior analysis program.
+ * ________________________________________________________________________________
+ *
+ * To work on Document Generation from — {.txt/.json} formatted Data.
+ * @feature   : charset="utf-8".
+ * ________________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ________________________________________________________________________________
+ *
+ * @date      : 11-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Babel
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ________________________________________________________________________________
+ *
+ * --/The Heart of Document Build System/-- of "MeltXpert®".
+ * ________________________________________________________________________________
+ */
